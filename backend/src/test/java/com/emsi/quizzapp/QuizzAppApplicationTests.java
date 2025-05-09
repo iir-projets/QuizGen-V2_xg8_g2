8e99999b7844240044e2632f4e17331a7a5cf71f
NisrineLachguer <nisrinelachguer37@gmail.com>
2025-05-09 05:25:30 +0100

    bug

--- backend/src/test/java/com/emsi/quizzapp/QuizzAppApplicationTests.java
+++ backend/src/test/java/com/emsi/quizzapp/QuizzAppApplicationTests.java
@@ -2,21 +2,12 @@ package com.emsi.quizzapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 class QuizzAppApplicationTests {
-
-    @Container
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test");
 
     @Test
     void contextLoads() {
     }
+
 }
