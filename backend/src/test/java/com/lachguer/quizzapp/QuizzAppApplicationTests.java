c1cfae7a350a08d5feeca6d87e54cb116e39967b
NisrineLachguer <135698847+NisrineLachguer@users.noreply.github.com>
2025-05-09 03:45:33 +0100

    Update QuizzAppApplicationTests.java

--- backend/src/test/java/com/lachguer/quizzapp/QuizzAppApplicationTests.java
+++ backend/src/test/java/com/lachguer/quizzapp/QuizzAppApplicationTests.java
@@ -1,13 +1,37 @@
-package com.lachguer.quizzapp;
+package com.emsi.quizzapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootTest
-class QuizzAppApplicationTests {
+@SpringBootApplication
+public class QuizzAppApplication {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzAppApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder.featuresToEnable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
 }
